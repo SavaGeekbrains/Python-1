0fa25d8d79d9291e2cf2d08c3a892369e0632a9f
Alexander Savchenko <Savchenko@live.ru>
2020-04-22 20:17:02 +0300

    Урок 3

---
+++ Домашняя работа/Урок 3/Задание 3.2.py
@@ -0,0 +1,6 @@
+def my_func(name, surname, byear, city, email, phone):
+    print(name, surname, byear, city, email, phone)
+
+
+my_func(name="Александр", surname="Савченко", byear = 1985, city="Ессентуки", email="email", phone="45454")
+
